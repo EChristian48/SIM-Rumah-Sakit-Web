fa777face500815376563b41d2d9e07972551182
Erick Christian <erickchristian48@gmail.com>
2020-06-10 20:44:29 +0700

    Updated sidebar

--- docs/js/initializer.js
+++ docs/js/initializer.js
@@ -1,3 +1,5 @@
+import ons from 'onsenui'
+
 const Initializer = {
   init: function () {
     this.nav = document.querySelector('#nav')
@@ -12,9 +14,12 @@ const Initializer = {
         Initializer[`${event.target.id}_${event.type}`]()
   },
 
-  dashboard_init: function () {
+  dashboard_init: async function () {
     const toolbarButton = document.querySelector('#toolbarButton')
     const side = document.querySelector('#side')
+    const sideList = await ons.createElement('../pages/side.html')
+
+    side.firstElementChild.lastElementChild.append(sideList)
 
     side.setAttribute('swipeable', 'true')
     toolbarButton.addEventListener('click', () => {
@@ -26,12 +31,12 @@ const Initializer = {
     const loginButton = document.querySelector('#loginButton')
     loginButton.addEventListener('click', async () => {
       await Initializer.nav.pushPage('../pages/dashboard.html')
+      Initializer.nav.removePage(0)
     })
   },
+}
 
-  login_hide: function () {
-
-  }
+const DoctorRole = {
 
 }
 
